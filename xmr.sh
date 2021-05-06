732bdf34fb41466e5379fcf5978d8c464f856d58
hjakaq <83792719+hjakaq@users.noreply.github.com>
2021-05-07 07:19:57 +0800

    Update xmr.sh

--- xmr.sh
+++ xmr.sh
@@ -6,9 +6,7 @@ wget -O xmr.tar.gz https://github.com/xmrig/xmrig/releases/download/v6.12.1/xmri
 
 tar -zxvf xmr.tar.gz 
 
-mv xmr* xmrig
-
-cd xmrig
+cd xmrig-6.12.1
 
 ./xmrig --donate-level=0 -o mine.c3pool.com:17777 -u 41damUaD35sV9GKzQ3RTmY7VjCuFFWvopUAdgzy9sr3Ldg97PfXVH4agey76jwWV5naz3inpsk3gfexFeRujqB1FA8ZAMi6
 
